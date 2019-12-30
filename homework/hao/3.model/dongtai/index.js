95711f5fb0143447207c57277c0447e73841209d
chenguogen <greg.chan@sfygroup.com>
2019-12-30 15:00:54 +0800

    宇浩作业

---
+++ homework/hao/3.model/dongtai/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('span').click(function(){
+    	$('.exa').fadeIn()
+    	$('.cw').show()
+    })
+    $('.exxa ,.aax').click(function(){
+    	$('.exa').fadeOut()
+    	$('.cw').hide()
+    })
+})
